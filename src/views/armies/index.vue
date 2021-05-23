d1f11a6b8381273604737b6ca4a589ff5a51fb23
mmilinkovic <mmilinkovic1992@gmail.com>
2021-05-24 01:33:35 +0200

    add component to list and search armies

--- src/views/armies/index.vue
+++ src/views/armies/index.vue
@@ -1,12 +1,63 @@
 <template>
   <div>
-    <h1>Fucking listing</h1>
+    <CCard>
+      <CCardBody>
+        <CRow>
+          <CCol sm="12">
+            <CForm inline>
+              <CInput
+                  class="mr-sm-2 my-2 my-sm-2"
+                  placeholder="Search"
+                  size="sm"
+                  ref="searchDetails"
+                  @keyup="searchArmies()"
+              />
+            </CForm>
+            <CTableWrapper
+                :items="armies"
+                hover
+                striped
+                border
+                small
+                fixed
+                caption="Armies"
+                :fields="fields"
+            />
+          </CCol>
+        </CRow>
+      </CCardBody>
+    </CCard>
   </div>
 </template>
 
 <script>
+
+import CTableWrapper from '../base/Table.vue'
+import Armies from "../../data/Armies";
+
 export default {
-  name: "index"
+  name: "index",
+  components: { CTableWrapper },
+  methods: {
+    searchArmies() {
+      const searchParam = this.$refs.searchDetails.state.toLowerCase()
+      this.armies = this.backupArmies
+      this.armies = this.armies.filter(army => army.name.toLowerCase().includes(searchParam))
+    }
+  },
+  data() {
+    return {
+      fields : ['name', 'units', 'attack_strategy', 'created_at', 'updated_at'],
+      armies: null,
+      backupArmies: null,
+      searchValue: ""
+    }
+  },
+  mounted() {
+    this.armies = Armies.methods.getArmies().armies
+    this.backupArmies = this.armies
+  }
+
 }
 </script>
 
