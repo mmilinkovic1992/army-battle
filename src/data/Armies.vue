e0df13e6289b2e3b6bc04879bdd89cb40fcca43c
mmilinkovic <mmilinkovic1992@gmail.com>
2021-05-24 01:33:05 +0200

    add Armies component to handle API requests

---
+++ src/data/Armies.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: "Armies",
+  data() {
+    return {
+      info: null
+    }
+  },
+  methods: {
+    getArmies() {
+        axios.get('http://army-battle.test/api/armies')
+             .then(response => (this.info = response))
+
+      return this.info.data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
