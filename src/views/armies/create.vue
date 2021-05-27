2f95b080d793af6bd4b8f3093be8f6b6031b1a21
mmilinkovic <mmilinkovic1992@gmail.com>
2021-05-27 23:11:32 +0200

    add component for army create

--- src/views/armies/create.vue
+++ src/views/armies/create.vue
@@ -1,12 +1,57 @@
 <template>
   <div>
-    <h1>Create army</h1>
+    <CCard>
+      <CCardHeader>
+        <strong>Company </strong><small>Form</small>
+      </CCardHeader>
+      <CCardBody>
+        <CInput
+            label="Name"
+            placeholder="Enter army name"
+            ref="army_name"
+        />
+        <CInput
+            label="Units"
+            placeholder="Number of units"
+            ref="units"
+            type="number"
+            min="1"
+            max="100"
+        />
+        <CSelect
+            label="Attack Strategy"
+            :options="{1: 'Select....', 2: 'random', 3: 'strongest', 4: 'weakest'}"
+            ref="attack_strategy"
+        />
+      </CCardBody>
+      <CCardFooter>
+        <CButton type="submit" size="sm" color="primary" @click="save"><CIcon name="cil-check-circle"/> Submit</CButton>
+        <CButton type="reset" size="sm" color="danger" @click="reset"><CIcon name="cil-ban"/> Reset</CButton>
+      </CCardFooter>
+    </CCard>
   </div>
 </template>
 
 <script>
+import Armies from "../../data/Armies";
+
 export default {
-  name: "create"
+  name: "create",
+  methods: {
+    reset() {
+      this.$refs.army_name.state = ""
+      this.$refs.units.state = ""
+      // this.$refs.attack_strategy.state = ""
+    },
+    save () {
+      const name = this.$refs.army_name.state
+      const units = this.$refs.units.state.toLowerCase()
+      const attackStrategy = this.$refs.attack_strategy.state.toLowerCase()
+      //Kad koristis dropdown, uzmi state odnosno naziv opcije, pa posle pronadji u nizu i uzmi key ako ti treba.
+      const info = Armies.methods.store({name: name, units: units, attack_strategy: attackStrategy})
+      console.log(info)
+    }
+  }
 }
 </script>
 
