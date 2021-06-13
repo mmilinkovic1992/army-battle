b8f7e4f95b9df274956e53dc2fe1c1e0bc30a615
Nemanja Danev <nemanjadanev@Nemanjas-Mac-mini.local>
2021-06-13 13:28:51 +0200

    redesign game logs and added pop up after armys win.

--- src/views/game/game-list.vue
+++ src/views/game/game-list.vue
@@ -6,22 +6,27 @@
         <div class="row">
           <div class="offset-md-2"></div>
           <div class="col-md-8">
+            <div class="message-error" v-if="messageError">{{ messageError }} <i class="fas fa-times" @click="closeMessage"></i></div>
+            <div class="message-success" v-if="messageSuccess">{{ messageSuccess }} <i class="fas fa-times" @click="closeMessage"></i></div>
             <form @submit.prevent="submit" class="form">
               <div class="form-group">
+                <label>Choose a game</label>
                 <select class="form-control" v-model="choosedGameId">
                   <option :value="game.id" v-for="game in games" :key="game.id">{{ game.name }}</option>
                 </select>
               </div>
               <div class="form-group text-center">
                 <button type="submit" class="game-btn ml-0"><i class="far fa-hand-point-right"></i> Run Attack</button>
+                <button class="game-btn" @click.prevent="resetGame">Reset Game</button>
               </div>
             </form>
             <div v-if="playGame">
               <div class="army-card">
+                <div class="attack-counter text-center">Attack Counter: <span>{{ countAttack }}</span></div>
                 <div class="army-title">Armies</div>
                 <ul class="army-list">
-                  <li v-for="(armies, index) in playGame.armies" :key="index" class="army-item" :class="[armies.units == 0 ? 'bg-red' : 'bg-green']">
-                    <div><i class="fas fa-fighter-jet"></i> {{armies.name}} - <span>{{armies.units}}</span> <span v-if="armies.units == 0 ? '' : 'WINNER'"></span></div>
+                  <li v-for="(armies, index) in playGame.armies" :key="index" class="army-item" :class="[armies.units != 0 ? 'bg-green' : 'bg-red']">
+                    <div><i class="fas fa-fighter-jet"></i> {{armies.name}} - <span>{{armies.units}}</span></div>
                   </li>
                 </ul>
               </div>
@@ -60,7 +65,7 @@
     <modal name="modal">
       <div v-if="playGame">
           <div v-for="(armie, index) in playGame.armies" :key="index">
-            <div class="text-center font-bold" v-if="armie.units !== 0">{{ armie.name }} WINNER!</div>
+            <div class="text-center winner" v-if="armie.units !== 0">{{ armie.name }} <span>WINS</span> after the {{ countAttack + 1 }}th attack.</div>
           </div>
       </div>
     </modal>
@@ -71,7 +76,6 @@
 import TopBar from './components/top-bar'
 import axios from 'axios'
 import 'babel-polyfill';
-import modal from 'vue-js-modal'
 export default {
   name: "game-list",
   components: {
@@ -82,12 +86,15 @@ export default {
       games: null,
       choosedGameId: null,
       playGame: null,
+      messageError: '',
+      messageSuccess: '',
+      countAttack: null
     }
   },
   async mounted() {
     try {
-      const resGames = await axios.get('http://army-battle.test/api/games');
-      this.games = resGames.data.games
+      const res = await axios.get('http://army-battle.test/api/games');
+      this.games = res.data.games
     } catch (error) {
       console.log(error)
     }
@@ -97,21 +104,51 @@ export default {
       const data = {
         game_id: this.choosedGameId,
       }
+      if(data.game_id !== null) {
+        const res = await axios.post('http://army-battle.test/api/game/run-attack', data);
+        this.playGame = res.data.game;
+      }else {
+        this.messageError = 'Choose a game to run attack.'
+      }
 
-      const res = await axios.post('http://army-battle.test/api/game/run-attack', data);
-      this.playGame = res.data.game;
+      if(this.playGame.status === 'started') {
+        this.countAttack++
+      }
 
-      console.log(this.countAttack)
+      if(this.playGame.status === 'finished') {
+        this.$modal.show('modal')
+      } else {
+        this.$modal.hide('modal')
+      }
       console.log(this.playGame)
-
-      window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
       
+      // window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
+          
     },
-    showModal(){
-      this.$modal.show('modal');
+    closeMessage() {
+      if(this.messageError) {
+        this.messageError = '';
+      }
+
+      if(this.messageSuccess) {
+        this.messageSuccess = '';
+      }
+
     },
-    hideModal(){
-        this.$modal.hide('modal');
+    async resetGame() {
+      const data = {
+        game_id: this.choosedGameId
+      }
+
+      if(data.game_id) {
+        const res = await axios.post('http://army-battle.test/api/game/reset/' + data.game_id)
+        this.games = null
+        this.playGame = null
+        this.$router.push("/game/add-army");
+
+      } else {
+        this.messageError = 'Choose a game to run attack.'
+      }
     }
   }
 }
