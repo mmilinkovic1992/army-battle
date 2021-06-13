49e6c5691b268f808d1e774e6b03613563898317
mmilinkovic <mmilinkovic1992@gmail.com>
2021-06-13 18:22:26 +0200

    add for errors and start pagination

--- src/views/armies/update.vue
+++ src/views/armies/update.vue
@@ -2,6 +2,16 @@
   <div>
     <h1>Update army</h1>
 
+    <div class="col-sm-6 col-md-12" v-if="this.info != ''">
+      <div class="card text-center bg-success text-white">
+        <div class="card-body">
+          <blockquote class="card-blockquote">
+            <p>{{ this.info }}</p>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+
     <CInput
         class="mr-sm-2 my-2 my-sm-2"
         placeholder="Search"
@@ -12,7 +22,7 @@
 
     <table class="table table-striped table-fixed table-hover table-bordered">
       <th>Name</th><th>Units</th><th>Attack Strategy</th><th>Option</th>
-      <tr v-for="item in this.armies">
+      <tr v-for="(item,index) in this.armies" :key="item.id">
         <td>
           {{item.name}}
         </td>
@@ -27,11 +37,40 @@
             Update
           </router-link>
 
-          <a class="btn btn-outline-danger btn-block active" @click="deleteArmy(item.id)">Delete</a>
+          <a class="btn btn-outline-danger btn-block active" @click="deleteArmy(item.id, index)">Delete</a>
 
         </td>
       </tr>
     </table>
+<!--    <nav aria-label="pagination">-->
+<!--      <ul class="pagination  justify-content-start">-->
+<!--        <li class="page-item">-->
+<!--          <a aria-label="Go to first page" href="#" target="_self" class="page-link">«</a>-->
+<!--        </li>-->
+<!--        <li class="page-item">-->
+<!--          <a aria-label="Go to previous page" href="#" target="_self" class="page-link">‹</a>-->
+<!--        </li>&lt;!&ndash;&ndash;&gt;-->
+<!--        <li class="page-item">-->
+<!--          <a aria-label="Go to page 1" href="#" target="_self" class="page-link c-page-link-number"> 1 </a>-->
+<!--        </li>-->
+<!--        <li class="page-item">-->
+<!--          <a aria-label="Go to page 2" href="#" target="_self" class="page-link c-page-link-number"> 2 </a>-->
+<!--        </li>-->
+<!--        <li class="active page-item">-->
+<!--          <a aria-label="Current page 3" href="#" target="_self" class="page-link c-page-link-number"> 3 </a>-->
+<!--        </li>-->
+<!--        <li class="page-item">-->
+<!--          <a aria-label="Go to page 4" href="#" target="_self" class="page-link c-page-link-number"> 4 </a>-->
+<!--        </li>-->
+<!--        <li class="page-item">-->
+<!--          <a aria-label="Go to next page" href="#" target="_self" class="page-link">›</a>-->
+<!--        </li>-->
+<!--        <li class="page-item">-->
+<!--          <a aria-label="Go to last page" href="#" target="_self" class="page-link">»</a>-->
+<!--        </li>-->
+<!--      </ul>-->
+<!--    </nav>-->
+
 
   </div>
 </template>
@@ -39,7 +78,6 @@
 <script>
 import axios from 'axios'
 import 'babel-polyfill';
-import Armies from "../../data/Armies";
 
 export default {
   name: "update",
@@ -47,7 +85,8 @@ export default {
     return {
       armies: [],
       backupArmies: [],
-      info: "",
+      info: '',
+      perPage: 5,
     }
   },
   methods: {
@@ -57,20 +96,24 @@ export default {
       this.armies = this.armies.filter(army => army.name.toLowerCase().includes(searchParam))
     },
 
-    async deleteArmy(id) {
-      try {
-        axios.delete('http://army-battle.test/api/armies/' + id + '/delete')
-            .then((response) => {
-              this.info = response
-            });
+    deleteArmy(id, index) {
+      this.index = index
+      let message;
+      axios({
+        method: 'delete',
+        url: 'http://army-battle.test/api/armies/'+ id +'/delete'
+      }).then(response => {
+        this.info = response.data.message
+      }).catch(function (error) {
+        console.log(error);
+      });
 
-        this.armies.filter((item) => item.id != id)
-        this.backupArmies.filter((item) => item.id != id)
-
-        // alert(response.message)
-      } catch (error) {
-        console.log(error)
-      }
+      this.deleteResponse(message, index)
+    },
+    deleteResponse(message, index) {
+      this.armies.splice(index, 1)
+      this.backupArmies.splice(index, 1)
+      setTimeout(() => this.info = "", 1500);
     }
   },
   async mounted() {
